843bc70ccae87e1e8061c512ab8d2958a077457a
Ram <gajjalaramprasadreddy@gmail.com>
2024-06-14 01:03:23 -0500

    latest sdk changes

--- Clickzin_ReactNative_Android/App.tsx
+++ Clickzin_ReactNative_Android/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, NativeModules} from 'react-native';
+import { NativeModules, Text, TouchableOpacity, View } from 'react-native';
 
-const{ClickzinAndroidTracker} = NativeModules
+const {ClickzinAndroidTracker} = NativeModules;
 
 const HelloWorldApp = () => {
   const handleButtonClick = () => {
-
-    ClickzinAndroidTracker.trackEvent("apikey","event",(err,message)=>{
-      if(err) return console.log(err)
-      console.log('message',message)
-
-    })
+    ClickzinAndroidTracker.trackEvent('apikey', 'event', (err, message) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('message', message);
+    });
   };
 
   return (
@@ -21,9 +21,9 @@ const HelloWorldApp = () => {
         alignItems: 'center',
       }}>
       <Text>Hello, world!</Text>
-      <TouchableOpacity onPress={handleButtonClick} style={{ marginTop: 20 }}>
-        <View style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5 }}>
-          <Text style={{ color: 'white' }}>Click Me</Text>
+      <TouchableOpacity onPress={handleButtonClick} style={{marginTop: 20}}>
+        <View style={{backgroundColor: 'blue', padding: 10, borderRadius: 5}}>
+          <Text style={{color: 'white'}}>Click Me</Text>
         </View>
       </TouchableOpacity>
     </View>
